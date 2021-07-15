c4f5e9e79fff89950c717fd85458f26e38442358
David Thol <david.thol@posteo.de>
2021-07-15 20:11:15 +0200

    Running server including static hosting of build

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,36 @@
 import Koa from "koa";
+import BodyParser = require("koa-bodyparser");
+import cors = require("koa-cors");
+import Logger = require("koa-logger");
+import mount = require("koa-mount");
+import Router = require("koa-router");
+import serve = require("koa-static");
+import HttpStatus from "http-status";
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = "Hello World";
+const static_pages = new Koa();
+static_pages.use(serve(__dirname + "/../frontend/build")); //serve the build directory
+app.use(mount("/", static_pages));
+
+const PORT = process.env.PORT || 3010;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get("/book",async (ctx,next)=>{
+  const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
+  ctx.status = HttpStatus.OK;
+  ctx.body = books;
+  await next();
 });
 
-app.listen(process.env.PORT ?? 3010, undefined, () => console.log(`Listening on port ${process.env.PORT}`));
+app.use(router.routes()).use(router.allowedMethods());
+
+
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+});
