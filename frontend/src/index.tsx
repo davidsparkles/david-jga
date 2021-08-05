f6c5fc3e12cd4b291f945621d17ba31daa4bc4ad
David Thol <david.thol@posteo.de>
2021-08-05 21:05:08 +0200

    nice menu

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Route path="/" exact component={() => <Redirect to="/quests" />} />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
